9a8c83bafb60c0e7ae09cf44dd9c5d59315e92c5
Phillip Schichtel <phillip@schich.tel>
2022-12-22 01:51:14 +0100

    verify option parsing for *_pub_* functions too

--- src/test/kotlin/tel/schich/pgcryptokt/OptionParsingTests.kt
+++ src/test/kotlin/tel/schich/pgcryptokt/OptionParsingTests.kt
@@ -18,5 +18,18 @@ class OptionParsingTests {
         assertThrows<IllegalStateException> {
             pgp_sym_decrypt_bytea("some test".toByteArray(), "password", "cipher=aes256")
         }
+
+        assertThrows<IllegalStateException> {
+            pgp_pub_encrypt("some test", "password".toByteArray(), "cipher=aes256")
+        }
+        assertThrows<IllegalStateException> {
+            pgp_pub_encrypt_bytea("some test".toByteArray(), "password".toByteArray(), "cipher=aes256")
+        }
+        assertThrows<IllegalStateException> {
+            pgp_pub_decrypt("some test".toByteArray(), "password".toByteArray(), options = "cipher=aes256")
+        }
+        assertThrows<IllegalStateException> {
+            pgp_pub_decrypt_bytea("some test".toByteArray(), "password".toByteArray(), options = "cipher=aes256")
+        }
     }
 }
