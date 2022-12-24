213f07c720a4284f2476170c26e3989eb9d95f6f
Phillip Schichtel <phillip@schich.tel>
2022-12-24 03:16:47 +0100

    implement armor related function from pgcrypto

---
+++ src/main/kotlin/tel/schich/pgcryptokt/armor.kt
@@ -0,0 +1,51 @@
+package tel.schich.pgcryptokt
+
+import org.bouncycastle.bcpg.ArmoredInputStream
+import org.bouncycastle.bcpg.ArmoredOutputStream
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+fun armor(data: ByteArray, keys: Array<String> = emptyArray(), values: Array<String>): String {
+    if (keys.size != values.size) {
+        throw IllegalArgumentException("keys (${keys.size} items) and values (${values.size} items) must have an equal amount of items")
+    }
+    val output = ByteArrayOutputStream()
+    val stream = ArmoredOutputStream(output)
+    for (i in keys.indices) {
+        stream.addHeader(keys[i], values[i])
+    }
+    stream.write(data)
+    stream.flush()
+    stream.close()
+    output.close()
+    return String(output.toByteArray())
+}
+
+fun dearmor(data: String): ByteArray {
+    val output = ByteArrayOutputStream()
+    val stream = ArmoredInputStream(ByteArrayInputStream(data.toByteArray()))
+    stream.copyTo(output)
+    stream.close()
+    output.close()
+    stream.close()
+    return output.toByteArray()
+}
+
+fun pgp_armor_headers(data: String, keys: MutableList<String>? = null, values: MutableList<String>? = null): List<Pair<String, String>> {
+    return ArmoredInputStream(ByteArrayInputStream(data.toByteArray())).use { stream ->
+        val headers = stream.armorHeaders ?: return emptyList()
+        buildList {
+            for (armorHeader in headers) {
+                val colonPosition = armorHeader.indexOf(':')
+                if (colonPosition == -1) {
+                    error("Received invalid header: $armorHeader")
+                }
+                val key = armorHeader.substring(0, colonPosition)
+                keys?.add(key)
+                val value = armorHeader.substring(colonPosition + 2) // 2 for colon and space
+                values?.add(value)
+                add(Pair(key, value))
+            }
+        }
+    }
+}
